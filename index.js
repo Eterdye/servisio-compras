5f43a1033c4b675822b9b7d63d88e30e4300cb0f
jova <jjcm@gmail.com>
2023-12-18 17:40:07 -0400

    inicializacion del servidor

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const appInit = express()
+const path = require('path')
+
+
+appInit.use(express.static(path.join(__dirname, 'front')))
+
+appInit.get('/', (req,res) => res.sendFile(path.join(__dirname, 'front/public/index.html')))
+
+
+appInit.get('/invento', (req,res) => {
+    const body = {
+        head: '0,0',
+        body: '( _ )',
+        feet: 'LL'
+    }
+
+    res.json(body)
+
+} )
+
+
+appInit.listen(8080)
