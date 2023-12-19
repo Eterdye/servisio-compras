6a73e46e0760e79430b115c457928379bb7e3c18
jova <jjcm@gmail.com>
2023-12-18 20:20:00 -0400

    cambio generico

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const path = require('path')
 
 appInit.use(express.static(path.join(__dirname, 'front')))
 
+
+let comentario_para_daniela= 'daniela mamalo'
+
 appInit.get('/', (req,res) => res.sendFile(path.join(__dirname, 'front/public/index.html')))
 
 
