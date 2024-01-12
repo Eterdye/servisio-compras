c205a2eacc91ab4637aa38c96e3e5e14a7d05914
Junior Corales <jjcmjavascript@gmail.com>
2024-01-12 18:31:59 -0300

    spa

--- front/src/general.js
+++ front/src/general.js
@@ -1,19 +1,28 @@
 const buttons = document.querySelectorAll('.link-nav');
 
-
-function updateToSection(evt) {
-  const targetButton = evt ? evt.target : buttons[0];
-  const sectionName = targetButton.name;
-  const mainSection = document.querySelector('#mainSection');
-
-  fetch(sectionName)
-  .then(res => res.text())
-  .then(html => mainSection.innerHTML = html )
-
+function updateToSection(sectionName) {
+  fetch(sectionName, {
+    headers: {
+      'Content-Type': 'text/html'
+    },
+    method: 'POST'
+  })
+    .then(res => res.text())
+    .then(html => mainSection.innerHTML = html )
 }
 
 document.addEventListener('DOMContentLoaded', function () {
-  updateToSection();
+  const sectionName = window.location.pathname.replace('\/', '') || 'inicio';
+
+  updateToSection(sectionName)
 });
 
-buttons.forEach((element) => element.addEventListener("click", updateToSection));
+buttons.forEach((element) => element.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  const sectionName = event.target.name;
+
+  updateToSection(sectionName);
+
+  history.pushState(null, null, sectionName);
+}));
--- routes.js
+++ routes.js
@@ -1,9 +1,14 @@
 const routes = [
-  { httpMethod: 'get', name: '/contacto', route: 'front/public/contacto.html'},
-  { httpMethod: 'get', name: '/detalles', route: 'front/public/detalles.html'},
   { httpMethod: 'get', name: '/', route: 'front/public/index.html'},
-  { httpMethod: 'get', name: '/inicio', route: 'front/public/inicio.html'},
-  { httpMethod: 'get', name: '/pedidos', route: 'front/public/pedidos.html'}
+  { httpMethod: 'get', name: '/contacto', route: 'front/public/index.html'},
+  { httpMethod: 'get', name: '/detalles', route: 'front/public/index.html'},
+  { httpMethod: 'get', name: '/inicio', route: 'front/public/index.html'},
+  { httpMethod: 'get', name: '/pedidos', route: 'front/public/index.html'},
+
+  { httpMethod: 'post', name: '/contacto', route: 'front/public/contacto.html'},
+  { httpMethod: 'post', name: '/detalles', route: 'front/public/detalles.html'},
+  { httpMethod: 'post', name: '/inicio', route: 'front/public/inicio.html'},
+  { httpMethod: 'post', name: '/pedidos', route: 'front/public/pedidos.html'}
 ]
 
 module.exports = routes
