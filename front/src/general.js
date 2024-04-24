2e2594500c96432f2d25a5e9266107c19bada6be
Eterdye <jovanny.idcorales@gmail.com>
2024-04-24 07:58:06 -0400

    last change

--- front/src/general.js
+++ front/src/general.js
@@ -20,12 +20,16 @@ function updateToSection(sectionName) {
       mainSection.innerHTML = html;
       if(sectionName === 'pedidos'){
         let order = document.getElementById('lista-de-productos')
-        let shoppingCart = document.getElementById('#shoppingCart')
-        requireShopingDiv(shoppingCart)
         requireProducts(order)
-      } else if (sectionName = 'inicio'){
+      }  else if (sectionName === 'compras'){
+          let shoppingCart = document.querySelector('#shoppingCart')
+          console.log(shoppingCart)
+        } 
+      
+
+
         
-      }
+
       // Elimina la clase 'section-transition' después de un breve retraso para activar la transición
       setTimeout(() => {
         mainSection.classList.remove('section-transition');
--- front/src/trolley.js
+++ front/src/trolley.js
@@ -4,18 +4,9 @@ const cart = []
 
 export default function addProduct(evt,event2){
     let trolley = document.querySelector('#trolley')
-    let shoppingCart = document.querySelector("#shoppingCart")
     trolley.classList.add('trolleyOn')
 
-    console.log(shoppingCart)
 
-let product = `
-<div class="product">
-<input type="number" class="productQuantity">
-<button>Quitar producto</button>
-</div>
-`
-    
 cart.push(allProducts.find(product => product.id == event2.getAttribute("primarykey")))
 
 console.log(cart)
